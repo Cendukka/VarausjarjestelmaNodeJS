55d130a20cef05c7c1af3eaf07b36d828262eefb
Oetoekkae <aleksi.kouhi@edu.turkuamk.fi>
2019-11-25 13:45:33 +0200

    DB connetcion, nodemon start command

--- main.js
+++ main.js
@@ -2,9 +2,22 @@
 
 const express = require("express");
 const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const port = 3000;
 const app = express();
+
+mongoose.connect(
+    "mongodb://localhost:27017/varausjarjestelma",
+    { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!")
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
