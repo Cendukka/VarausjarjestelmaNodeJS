e5901242057a9de698718c324b43c2034b67f10f
Cendukka <32675104+Cendukka@users.noreply.github.com>
2019-12-05 16:00:33 +0200

    Update homeController.js
    correct routes renders correct views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,3 +4,18 @@
 exports.showHome = (req, res) => {
     res.render("index");
 };
+exports.showLogin = (req, res) => {
+    res.render("login");
+};
+exports.showReserve = (req, res) => {
+    res.render("varaa");
+};
+exports.showReservations = (req, res) => {
+    res.render("varaukset");
+};
+exports.showContacts = (req, res) => {
+    res.render("yhteystiedot");
+};
+exports.showRules = (req, res) => {
+    res.render("saannot");
+};
