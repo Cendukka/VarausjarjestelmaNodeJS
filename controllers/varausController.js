fec84e78c45433f2018041fbeeb66cbbeb565a71
Cendukka <32675104+Cendukka@users.noreply.github.com>
2019-12-06 22:20:58 +0200

    Update varausController.js
    Added methods to format the date so it will show it in readable format.

--- controllers/varausController.js
+++ controllers/varausController.js
@@ -1,14 +1,39 @@
 "use strict";
 
 const varausModel = require("../models/varaus");
+const moment = require('moment');
 
-
+//Fetches all reservations from the database and then renders the page to show them
 exports.getAllReservations = (req, res) => {
   varausModel.find({})
     .exec()
     .then(varaus => {
+
+      //create empty lists
+      let reservers = [];
+      let sports = [];
+      let dates = [];
+      let startTimes = [];
+      let endTimes = [];
+
+      //Loop each row from the database collection and 
+      varaus.forEach(v =>{
+        reservers.push(v.name);
+        sports.push(v.sport);
+        let unformatedDate = moment(v.date);
+        let formatedDate = unformatedDate.format("DD/MM/YYYY");
+        dates.push(formatedDate);
+        startTimes.push(v.startTime);
+        endTimes.push(v.endTime);
+      });
+
+      //after all has been pushed, let's render the reservation page, parameters are the filled lists
       res.render("varaukset", {
-        varaus: varaus
+        reservers: reservers,
+        sports: sports,
+        dates: dates,
+        startTimes: startTimes,
+        endTimes: endTimes
       });
     })
     .catch(error => {
@@ -24,6 +49,7 @@ exports.getReservationsPage = (req, res) => {
   res.render("varaukset");
 };
 
+//Creates new reservation object and saves in in the database
 exports.saveReservation = (req, res) => {
   
   let newVaraus = new varausModel({
@@ -36,7 +62,9 @@ exports.saveReservation = (req, res) => {
   newVaraus
     .save()
     .then(result => {
-      res.render("varaukset");
+      let unformatedDate = moment(result.date);
+      let formatedDate = unformatedDate.format("DD/MM/YYYY");
+      res.render("kiitos", { result: result, date: formatedDate});
     })
     .catch(error => {
       if (error) res.send(error);
