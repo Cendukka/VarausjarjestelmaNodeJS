c420570e92666690e06490594f963a48d4ed20f0
Cendukka <32675104+Cendukka@users.noreply.github.com>
2019-12-06 20:44:15 +0200

    Route additions
    Added routes for reservations and post route for reservation reserve's

--- routes/router.js
+++ routes/router.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require("../controllers/homeController");
+const varausController = require("../controllers/varausController");
 
 router.get("/", homeController.showHome);
 router.get("/login", homeController.showLogin);
 router.get("/reserve", homeController.showReserve);
-router.get("/reservations", homeController.showReservations);
+router.post("/reserve/new", varausController.saveReservation)
+router.get("/reservations", varausController.getAllReservations);
 router.get("/contacts", homeController.showContacts);
 router.get("/rules", homeController.showRules);
 
